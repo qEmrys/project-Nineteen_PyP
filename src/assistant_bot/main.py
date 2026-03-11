d964b27460e90743626760f41d8f9c34c3acbdeb
Yuliia Varkhol <juljailchenko@gmail.com>
2026-03-11 22:45:04 +0200

    add functionality for adding notes, saving and showing list of the notes

--- src/assistant_bot/main.py
+++ src/assistant_bot/main.py
@@ -1,18 +1,28 @@
-from assistant_bot.storage.file_storage import load_data
-from assistant_bot.handlers.commands import COMMANDS
+from assistant_bot.storage.file_storage import load_data, load_notes, save_notes
+from assistant_bot.handlers.commands import COMMANDS, NOTE_COMMANDS
 from assistant_bot.utils.parser import parse_input
 
 
 def main():
     book = load_data()
+    notebook = load_notes()
+
     print("Welcome to the assistant bot!")
 
     while True:
         user_input = input("Enter a command: ")
         command, args = parse_input(user_input)
         handler = COMMANDS.get(command)
+        note_handler = NOTE_COMMANDS.get(command)
 
         if handler:
+            # Saving notes after the assistant bot closing 
+            if command in ("exit", "close"):
+                save_notes(notebook)
             print(handler(args, book))
+
+        elif note_handler:
+            print(note_handler(args, notebook))
+
         else:
             print("Invalid command.")
