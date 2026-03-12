19ae52cee234f32dac3a5b80ca6a12a8ed36a8b6
qEmrys <takashiro.sv@gmail.com>
2026-03-12 13:36:12 +0200

    refactor: introduce AssistantData and refactor storage, handlers, and commands

--- src/assistant_bot/storage/file_storage.py
+++ src/assistant_bot/storage/file_storage.py
@@ -1,38 +1,23 @@
 import pickle
 from pathlib import Path
-from assistant_bot.models.address_book import AddressBook
-from assistant_bot.models.note_book import NoteBook
+from assistant_bot.models.assistant_data import AssistantData
 
 BASE_DIR = Path(__file__).resolve().parents[3]
-DATA_FILE = BASE_DIR / "data" / "addressbook.pkl"
-NOTES_FILE = BASE_DIR / "data" / "notebook.pkl"
+DATA_DIR = BASE_DIR / "data"
+DATA_FILE = DATA_DIR / "assistant.pkl"
 
 DATA_FILE.parent.mkdir(parents=True, exist_ok=True)
 
 
-def save_data(book, filename=DATA_FILE):
+def save_data(data: AssistantData, filename: Path = DATA_FILE) -> None:
+    # Серіалізує AssistantData у файл через pickle.
     with open(filename, "wb") as f:
-        pickle.dump(book, f)
-
+        pickle.dump(data, f)
 
 def load_data(filename=DATA_FILE):
+    # Завантажує AssistantData з файлу.
     try:
         with open(filename, "rb") as f:
             return pickle.load(f)
     except FileNotFoundError:
-        return AddressBook()
-
-def save_notes(notebook: NoteBook, filename: Path = NOTES_FILE) -> None:
-    #Серіалізує NoteBook у файл через pickle.
-    with open(filename, "wb") as f:
-        pickle.dump(notebook, f)
-
-def load_notes(filename: Path = NOTES_FILE) -> NoteBook:
-    #Завантажує NoteBook з файлу.
-    #Якщо файл ще не існує — повертає порожню книгу нотаток.
-    
-    try:
-        with open(filename, "rb") as f:
-            return pickle.load(f)
-    except FileNotFoundError:
-        return NoteBook()
+        return AssistantData()
